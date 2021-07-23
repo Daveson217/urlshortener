a4c539158b27cc99b66e3dfaa81c8d8799cbafc6
David Babalola <oredavid14@gmail.com>
2021-07-23 18:29:27 +0100

    Some Heroku config

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminB23/', admin.site.urls),
     path('', include('shortener.urls')),
 ]
